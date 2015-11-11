90d4590cd4ab650738c0ece487ec3ffd47ed24ef
zhzhang <zhzhang@thoughtworks.com>
2015-11-11 20:09:24 +0800

    change react and react router version

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
 var App = require('./components/App');
-ReactDOM.render(<App />, document.getElementById('itemList'));
+React.render(<App />, document.getElementById('itemList'));
 
