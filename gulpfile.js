5cdb7c3e1b1fc9325cc89a410058b8a18483f079
zhzhang <zhzhang@thoughtworks.com>
2015-11-11 19:47:02 +0800

    install gulp plugins using npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,56 @@
-// Load some modules which are installed through NPM.
+'use strict';
 var gulp = require('gulp');
-var browserify = require('browserify');  // Bundles JS.
-var del = require('del');  // Deletes files.
-var reactify = require('reactify');  // Transforms React JSX to JS.
+var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var del = require('del');
+var reactify = require('reactify');
 var source = require('vinyl-source-stream');
-var stylus = require('gulp-stylus');  // To compile Stylus CSS.
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-// Define some paths.
 var paths = {
-    css: ['src/css/**/*.styl'],
-    app_js: ['./src/js/app.jsx'],
-    js: ['src/js/*.js'],
+    less: ['./src/style/**/*.less'],
+    jade: ['./src/index.jade'],
+    mainJs: ['./src/js/main.js'],
+    js: ['./src/js/*.js']
 };
 
-// An example of a dependency task, it will be run before the css/js tasks.
-// Dependency tasks should call the callback to tell the parent task that
-// they're done.
 gulp.task('clean', function(done) {
     del(['build'], done);
 });
 
-// Our CSS task. It finds all our Stylus files and compiles them.
-gulp.task('css', ['clean'], function() {
-    return gulp.src(paths.css)
-        .pipe(stylus())
-        .pipe(gulp.dest('./src/css'));
+gulp.task('lessCompiler', function() {
+    return gulp.src(paths.less)
+        .pipe($.less())
+        .pipe(gulp.dest('build/style'));
 });
 
-// Our JS task. It will Browserify our code and compile React JSX files.
-gulp.task('js', ['clean'], function() {
-    // Browserify/bundle the JS.
-    browserify(paths.app_js)
+gulp.task('jadeCompiler', function () {
+    return gulp.src(paths.jade)
+        .pipe($.jade({pretty: true}))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('bundleJs', function(){
+    browserify(paths.mainJs)
         .transform(reactify)
         .bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./src/'));
+        .pipe(gulp.dest('build'));
 });
 
-// Rerun tasks whenever a file changes.
-gulp.task('watch', function() {
-    gulp.watch(paths.css, ['css']);
-    gulp.watch(paths.js, ['js']);
+gulp.task('serve', ['bundleJs', 'lessCompiler', 'jadeCompiler'], function () {
+    browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: 'build'
+        }
+    });
+
+    gulp.watch(paths.less, ['lessCompiler', reload]);
+    gulp.watch(paths.js, ['bundleJs', reload]);
+    gulp.watch(paths.jade, ['jadeCompiler', reload]);
 });
 
-// The default task (called when we run `gulp` from cli)
-gulp.task('default', ['watch', 'css', 'js']);
+gulp.task('default', ['serve']);
