91b3dc3b9d6a89d4794699c3c0603caec66dbc76
zhzhang <zhzhang@thoughtworks.com>
2015-12-20 20:58:37 +0800

    use reactDOM instead of react

--- src/js/client.js
+++ src/js/client.js
@@ -1,8 +1,9 @@
 'use strict';
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Router = require('react-router');
 var routes = require('./routes');
 
 Router.run(routes, Router.HashLocation, function(Root){
-    React.render(<Root />, document.getElementById('itemList'));
+    ReactDOM.render(<Root />, document.getElementById('itemList'));
 });
